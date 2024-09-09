9d9c4ba3412140876011a709d3832572c616cff5
Asdfghjkkllljd <staricinal49@gmail.com>
2024-09-09 16:51:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-TOKEN = "6655387999:AAF0z5A0aApcHgMiGzOQya6d-E7i556oTPY" # Токен бота
-CHANNEL_ID = -1002183015926 # ID канала со ставками
-ADMINS = [6546701182] # ID админов
-BET_URL = "http://t.me/send?start=IV1m6PIjo80Q" # Ссылка на счет
-CRYPTOPAY_TOKEN = "199399:AAviFbgmrhlfiUlFSkSc2MIxnhCo96CpWSw" # Тут CryptoPay токен
+TOKEN = "7519672085:AAHE5njvHUHxBHvz7F31g9LbLkdQwcYJBFY" # Токен бота
+CHANNEL_ID = -1002243436081 # ID канала со ставками
+ADMINS = [5846969551] # ID админов
+BET_URL = "t.me/send?start=IVpAqLJ8Pztg" # Ссылка на счет
+CRYPTOPAY_TOKEN = "257530:AAjBbpqNi7zCgG9RQwAlTDLOFRdKySJ2N6Q" # Тут CryptoPay токен
 WIN_IMAGE = "win.jpeg" # Название файла с картинкой выигрыша
 LOSE_IMAGE = "lose.jpeg" # Название файла с картинкой проигрыша
-LOGS_ID = -1002208898118 # ID канала с оплатами
-RULES_LINK = "https://t.me/logsmon" # Ссылка на канал с правилами
-NEWS_LINK = "https://t.me/pestebin" # Ссылка на новостной канал
-OWNER_LINK = "https://t.me/ceolli" # Ссылка на владельца/поддержку
-PEREHOD_LINK = "https://t.me/MonsterBetCas" # Ссылка на переходник
-BETS_LINK = "https://t.me/smisq" # Ссылка на канал ставок
-CASINO_NAME = "MonsterBet" # Название казино
+LOGS_ID = -1002226990800 # ID канала с оплатами
+RULES_LINK = "https://t.me/+k42zVBd8Iz41ZDEy" # Ссылка на канал с правилами
+NEWS_LINK = "https://t.me/+YXO5t3AYIRgxZTdi" # Ссылка на новостной канал
+OWNER_LINK = "https://t.me/CEKC_HA_OCTPOBAX" # Ссылка на владельца/поддержку
+PEREHOD_LINK = "@extremecasinoo" # Ссылка на переходник
+BETS_LINK = "https://t.me/extremecasinoo" # Ссылка на канал ставок
+CASINO_NAME = "EXTREME CASINO" # Название казино
